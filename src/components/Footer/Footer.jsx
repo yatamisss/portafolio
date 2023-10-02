055402dfadd9ca4150db188a21637fbf787a2a93
yatamisss <yatamispo@gmail.com>
2023-10-02 17:56:01 -0300

    primeras funcionalidades del prototipo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="container-footer">
+                <div className="container-footer__informacion">
+                    <a href="https://github.com/yatamisss/proyectoFinalReact" target="_blank">Link a repositorio</a>
+                    <p>No nos contactes por aqui por favor</p>
+                    <p>@paginafake</p>
+                    <p>©todos los derechos reservados</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <nav className="nav-bar">
+                        <a className="nav-bar__a" href="">inicio</a>
+                        <a className="nav-bar__a" href="">videos mas populares</a>
+                        <form className="nav-bar__form" action="">
+                            <input type="search" placeholder="Encuentra mucho más" />
+                            <button type="submit">Buscar</button>
+                        </form>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
